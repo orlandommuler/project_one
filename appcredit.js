23506ad39b182666a201a54329c7f4d5f1cefb7b
orlandommuler <orlando.muler12@gmail.com>
2017-03-26 05:55:25 -0400

    commit

---
+++ appcredit.js
@@ -0,0 +1,14 @@
+﻿
+// just for the demos, avoids form submit
+jQuery.validator.setDefaults({
+  debug: true,
+  success: "valid"
+});
+$( "#myform" ).validate({
+  rules: {
+    field: {
+      required: true,
+      creditcard: true
+    }
+  }
+});
